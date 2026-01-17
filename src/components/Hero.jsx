271242787187832650bf00a8da0d605b08901a00
AryashYadav0 <aryashyadav0680@gmail.com>
2026-01-18 02:26:52 +0530

    implement hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,10 @@ const Hero = () => {
             <img src="/title.png" alt="MacBook Title" />
         </div>
 
-        <video src="/videos/hero.mp4" autoPlay muted playsInline />
+        <video ref={videoRef} src="/videos/hero.mp4" autoPlay muted playsInline />
+
+        <button>Buy</button>
+        <p>From $1599 or $133/mo for 12 months</p>
     </section>
   )
 }
