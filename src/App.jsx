ecd2d16622189182bd78c0a3feaa4296d2c7a5b1
AryashYadav0 <aryashyadav0680@gmail.com>
2026-01-20 14:58:49 +0530

    add showcase

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import NavBar from './components/NavBar'
 import ProductViewer from './components/ProductViewer'
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
+import ShowCase from './components/ShowCase';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -13,6 +14,7 @@ const App = () => {
       <NavBar/>
       <Hero/>
       <ProductViewer/>
+      <ShowCase/>
     </main>
   )
 }
